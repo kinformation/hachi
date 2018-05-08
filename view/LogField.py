a704a7de4a95d1d4af46aec781defe582c238edb
kinformation <kanatani.social@gmail.com>
2018-05-08 12:51:27 +0900

    ScrolledText

--- view/LogField.py
+++ view/LogField.py
@@ -1,30 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
-from tkinter import ttk
+from tkinter import scrolledtext
 
 from controller import LogController
 
 
 class LogField:
-    """ 表示領域の設定 """
-
+    """ ログ表示領域の設定 """
     def __init__(self, master):
         self._set_log_field(master)
 
     def _set_log_field(self, parent_frame):
-
-        frame = ttk.Frame(parent_frame)
-        frame.pack()
-
-        # ログ表示エリア
-        log_area = tk.Text(frame, height=8)
-        log_area.pack(fill=tk.BOTH, side=tk.LEFT)
+        log_area = scrolledtext.ScrolledText(parent_frame, height=8)
+        log_area.pack(fill=tk.BOTH)
         # ここでLogControllerを最初に呼ぶこと
         LogController.LogController(log_area)
-
-        # Scrollbar
-        scrollbar = ttk.Scrollbar(
-            frame, orient=tk.VERTICAL, command=log_area.yview)
-        log_area['yscrollcommand'] = scrollbar.set
-        scrollbar.pack(side=tk.LEFT, fill=tk.Y)
