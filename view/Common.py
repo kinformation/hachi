d42299fb7cc62d473d95dffe8d21f71eddef4616
Kanatani Kazuya <kanatani.social@gmail.com>
2018-05-19 09:08:12 +0900

    modify

--- view/Common.py
+++ view/Common.py
@@ -7,26 +7,27 @@ from tkinter import ttk
 class LabelEntry(ttk.Frame):
     """ ラベル付きEntry（横並び） """
 
-    def __init__(self, master=None, text=None, width=None, textvariable=None):
+    def __init__(self, master=None, text=None, **entry_kw):
         ttk.Frame.__init__(self, master)
         # Label
         self.Label = ttk.Label(self, text=text)
         self.Label.pack(side=tk.LEFT)
         # Entry
-        self.Entry = ttk.Entry(self, width=width, textvariable=textvariable)
+        self.Entry = ttk.Entry(self)
+        self.Entry.configure(entry_kw)
         self.Entry.pack()
 
 
 class LabelReadonlyEntry(ttk.Frame):
     """ ラベル付き非活性Entry（縦並び） """
 
-    def __init__(self, master=None, text=None, width=None, textvariable=None):
+    def __init__(self, master=None, text=None, **entry_kw):
         ttk.Frame.__init__(self, master)
         # Label
         self.Label = ttk.Label(self, text=text)
         self.Label.pack()
         # Entry
-        self.Entry = ttk.Entry(self, width=width,
-                               textvariable=textvariable, justify=tk.RIGHT)
+        self.Entry = ttk.Entry(self, justify=tk.RIGHT)
+        self.Entry.configure(entry_kw)
         self.Entry.state(['readonly'])
         self.Entry.pack()
